f2df7a697ba84d49bf54fd09710106b5a3b070a7
sorabh sharma <ssorabh.ssharma@gmail.com>
2018-07-02 21:10:11 +0530

    wordpress boilerplate

---
+++ wp-content/plugins/my-plugin-prac/my-plugin-prac.php
@@ -0,0 +1,18 @@
+<?php 
+
+/*
+Plugin Name: My Plugin Prac
+Description: Example practise plugin for "Wordpress Plugin Practise".
+Plugin URI: http://sorabh86.github.com
+Author: Sorabh
+Version: 1.0
+Text Domain: my-plugin-prac
+Domain Path: /languages
+License: GPL v2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.txt
+*/
+
+//exit if file is called directly
+if( !(defined('ABSPATH')) ) {
+	exit;
+}
